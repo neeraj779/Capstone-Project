cdbdcbebe50200458313b4909a36a3a158c82e17
Neeraj <neeraj79108@gmail.com>
2024-07-24 17:10:19 +0530

    Add ErrorModel class

---
+++ Swar.API/Models/ErrorModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Swar.API.Models
+{
+    public class ErrorModel
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+
+        public ErrorModel() { }
+
+        public ErrorModel(int statusCode, string message)
+        {
+            Status = statusCode;
+            Message = message;
+        }
+    }
+}
