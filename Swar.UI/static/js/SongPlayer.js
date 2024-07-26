e8e040246134c97dbea10b305943beb324a05127
Neeraj <neeraj79108@gmail.com>
2024-07-26 16:37:19 +0530

    feat: Update SongPlayer.js to display singers in addition to primary artists

--- Swar.UI/static/js/SongPlayer.js
+++ Swar.UI/static/js/SongPlayer.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       audio.src = data.media_url;
       document.getElementById("song-title").textContent = data.song;
       document.getElementById("song-artist").textContent =
-        data.primary_artists || "Unknown";
+        data.singers || data.primary_artists || "Unknown Artist";
       document.getElementById("song-image").src = data.image;
       document.getElementById("song-lyrics").innerHTML =
         data.lyrics || "Lyrics not available";
