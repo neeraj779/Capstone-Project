c181450e9276f40714ea1e47368b03b1485f4e4b
Neeraj <neeraj79108@gmail.com>
2024-07-27 10:42:00 +0530

    feat: Set mobile search input value to match desktop search input

--- Swar.UI/static/js/search.js
+++ Swar.UI/static/js/search.js
@@ -25,6 +25,7 @@ async function fetchSearchData(query) {
   try {
     const searchUrl = `https://songserviceapi.azurewebsites.net/api/SongsData/GetSongsByQuery?query=${query}&lyrics=false&songData=true`;
     document.getElementById("search-input").value = query;
+    document.getElementById("mobile-search-input").value = query;
     const response = await fetch(searchUrl);
     if (!response.ok) {
       throw new Error("Failed to fetch search data");
