cbf9b34afe83462a5184943ec9fd720185319f7b
Neeraj <neeraj79108@gmail.com>
2024-08-09 11:49:31 +0530

    Remove unused code and dependencies in AuthContext.jsx

--- Swar.UI.React/src/contexts/AuthContext.jsx
+++ Swar.UI.React/src/contexts/AuthContext.jsx
@@ -5,7 +5,6 @@ import {
   useMemo,
   useState,
 } from "react";
-import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
@@ -17,7 +16,6 @@ export const AuthProvider = ({ children }) => {
   const [refreshToken, setRefreshToken] = useState(() =>
     localStorage.getItem("refreshToken")
   );
-  const navigate = useNavigate();
 
   const updateLocalStorage = useCallback((key, value) => {
     if (value) {
@@ -34,92 +32,6 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     updateLocalStorage("refreshToken", refreshToken);
   }, [refreshToken, updateLocalStorage]);
-
-  const redirectToLogin = useCallback(() => {
-    navigate("/login");
-  }, [navigate]);
-
-  const verifyAccessToken = useCallback(
-    async (token) => {
-      try {
-        const response = await fetch(
-          "https://swarapi.azurewebsites.net/api/v1/users/verify-token",
-          {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        return response.ok;
-      } catch (error) {
-        console.error("Error verifying access token:", error);
-        redirectToLogin();
-        return false;
-      }
-    },
-    [redirectToLogin]
-  );
-
-  const refreshAccessToken = useCallback(
-    async (token) => {
-      try {
-        const response = await fetch(
-          "https://swarapi.azurewebsites.net/api/v1/users/refresh-token",
-          {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        if (response.ok) {
-          const data = await response.json();
-          setAccessToken(data.accessToken);
-          window.location.reload();
-        } else if (response.status === 401) {
-          console.warn("Refresh token is invalid or expired.");
-          redirectToLogin();
-        }
-      } catch (error) {
-        console.error("Error refreshing access token:", error);
-        redirectToLogin();
-      }
-    },
-    [redirectToLogin]
-  );
-
-  const checkAuth = useCallback(async () => {
-    if (!accessToken && !refreshToken) {
-      return redirectToLogin();
-    }
-
-    if (accessToken) {
-      const isValid = await verifyAccessToken(accessToken);
-      if (isValid) return;
-
-      if (refreshToken) {
-        await refreshAccessToken(refreshToken);
-      } else {
-        redirectToLogin();
-      }
-    } else if (refreshToken) {
-      await refreshAccessToken(refreshToken);
-    } else {
-      redirectToLogin();
-    }
-  }, [
-    accessToken,
-    refreshToken,
-    verifyAccessToken,
-    refreshAccessToken,
-    redirectToLogin,
-  ]);
-
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
 
   const contextValue = useMemo(
     () => ({
