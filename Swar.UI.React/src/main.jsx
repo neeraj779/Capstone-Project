a905b13779826defdeed5adac0253788db8432d7
Neeraj <neeraj79108@gmail.com>
2024-08-11 07:59:23 +0530

    chore: Update body background color in index.html and add main container in main.jsx

--- Swar.UI.React/src/main.jsx
+++ Swar.UI.React/src/main.jsx
@@ -7,7 +7,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <main className="bg-gray-900 min-h-screen">
+        <App />
+      </main>
     </NextUIProvider>
   </React.StrictMode>
 );
