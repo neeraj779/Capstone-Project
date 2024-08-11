63b1fc718c72555c3abe3f0cc2ffd1213403e8aa
Neeraj <neeraj79108@gmail.com>
2024-08-11 16:00:20 +0530

    Update ArtistButton component to use query parameter for search

--- Swar.UI.React/src/components/ArtistButton.jsx
+++ Swar.UI.React/src/components/ArtistButton.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const ArtistButton = ({ artist }) => {
   return (
     <Link
-      to={`/search/${encodeURIComponent(artist)}`}
-      className="bg-gray-700 text-white text-xs font-semibold py-2 px-4 rounded-full hover:bg-gray-600 transition duration-300"
+      to={`/search?q=${encodeURIComponent(artist)}`}
+      className="bg-gray-700 text-white text-xs font-semibold py-3 px-4 rounded-full hover:bg-gray-600 transition duration-300"
     >
       {artist}
     </Link>
