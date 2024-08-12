e3063dd8bd81fad5128403349a13fc775236c532
Neeraj <neeraj79108@gmail.com>
2024-08-12 15:28:54 +0530

    Update SearchBar component with new search icon

--- Swar.UI.React/src/components/SearchBar.jsx
+++ Swar.UI.React/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { FcSearch } from "react-icons/fc";
+import { FaSearch } from "react-icons/fa";
 
 const MobileSearchBar = () => {
   const navigate = useNavigate();
@@ -19,7 +19,7 @@ const MobileSearchBar = () => {
           className="bg-gray-700 text-white placeholder-gray-400 rounded-full py-2 px-4 pl-10 w-full"
           placeholder="Search songs, artists..."
         />
-        <FcSearch />
+        <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
       </form>
     </>
   );
