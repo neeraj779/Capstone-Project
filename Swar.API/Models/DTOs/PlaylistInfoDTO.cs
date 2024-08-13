fa4beb4754685f5d494a7c9c9cba76a817f15530
Neeraj <neeraj79108@gmail.com>
2024-08-13 19:29:05 +0530

    refactor: Update PlaylistInfoDTO

--- Swar.API/Models/DTOs/PlaylistInfoDTO.cs
+++ Swar.API/Models/DTOs/PlaylistInfoDTO.cs
@@ -1,14 +1,7 @@
 ﻿namespace Swar.API.Models.DTOs
 {
-    public class PlaylistInfoDTO
+    public class PlaylistInfoDTO : ReturnPlaylistDTO
     {
-        public int PlaylistId { get; set; }
-        public string PublicId { get; set; } = string.Empty;
-        public string OwnerName { get; set; } = string.Empty;
-        public string PlaylistName { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public bool IsPrivate { get; set; }
-        public DateTime CreatedAt { get; set; }
         public int SongsCount { get; set; }
     }
 }
