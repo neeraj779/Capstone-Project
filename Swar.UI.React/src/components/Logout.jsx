6d1c24e7bab240cf8308ec1e15487a34592dc0a6
Neeraj <neeraj79108@gmail.com>
2024-08-18 15:01:59 +0530

    refactor: Update Logout component to use smaller font size for heading

--- Swar.UI.React/src/components/Logout.jsx
+++ Swar.UI.React/src/components/Logout.jsx
@@ -18,7 +18,7 @@ const Logout = () => {
               className="mb-6"
             />
           </div>
-          <h1 className="text-4xl font-extrabold text-white">
+          <h1 className="text-2xl font-extrabold text-white">
             You’re Logged Out
           </h1>
           <p className="text-gray-400 mt-4">
