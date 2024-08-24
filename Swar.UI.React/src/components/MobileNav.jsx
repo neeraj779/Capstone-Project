a319bdf0c4c121e95522b4049413510181a5bec8
Neeraj <neeraj79108@gmail.com>
2024-08-24 19:41:34 +0530

    refactor: Update MobileNav icons to use react-icons/fa and react-icons/lu

--- Swar.UI.React/src/components/MobileNav.jsx
+++ Swar.UI.React/src/components/MobileNav.jsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
-import { House, Library, User } from "lucide-react";
+import { FaHome, FaUser } from "react-icons/fa";
+import { LuLibrary } from "react-icons/lu";
 
 const MobileNav = () => {
   return (
     <div className="fixed bottom-0 inset-x-0 bg-gray-800 text-white md:hidden z-20">
       <div className="flex justify-around items-center py-2">
         <Link to="/home" className="flex flex-col items-center">
-          <House />
+          <FaHome className="text-xl" />
           <span className="text-xs">Home</span>
         </Link>
         <Link to="/library" className="flex flex-col items-center">
-          <Library />
+          <LuLibrary className="text-xl" />
           <span className="text-xs">Library</span>
         </Link>
         <Link to="/profile" className="flex flex-col items-center">
-          <User />
+          <FaUser className="text-xl" />
           <span className="text-xs">Profile</span>
         </Link>
       </div>
