5223d254e111a9c728eb0adff67140abde9e2848
Neeraj <neeraj79108@gmail.com>
2024-08-25 12:44:44 +0530

    refactor: Update MobileNav icons

--- Swar.UI.React/src/components/MobileNav.jsx
+++ Swar.UI.React/src/components/MobileNav.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
-import { FaHome, FaUser } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Avatar } from "@nextui-org/react";
 
 const MobileNav = () => {
+  const { user } = useAuth0();
+
   return (
     <div className="fixed inset-x-0 bottom-0 z-20 text-white bg-gray-800 md:hidden">
       <div className="flex items-center justify-around py-2">
@@ -15,7 +19,7 @@ const MobileNav = () => {
           <span className="text-xs">Library</span>
         </Link>
         <Link to="/profile" className="flex flex-col items-center">
-          <FaUser className="text-xl" />
+          <Avatar src={user.picture} className="w-6 h-6" />
           <span className="text-xs">Profile</span>
         </Link>
       </div>
