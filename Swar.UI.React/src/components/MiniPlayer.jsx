5af1439346f38cd4f416a80d968c6dfa089f95f7
Neeraj <neeraj79108@gmail.com>
2024-08-25 11:15:27 +0530

    refactor: Improve MiniPlayer component UI

--- Swar.UI.React/src/components/MiniPlayer.jsx
+++ Swar.UI.React/src/components/MiniPlayer.jsx
@@ -20,18 +20,18 @@ const MiniPlayer = () => {
     return null;
 
   return (
-    <div className="fixed bottom-14 mb-1 md:bottom-0 left-1 right-1 bg-gray-800 text-white flex items-center p-2 shadow-lg border-t border-gray-700 rounded-lg z-20">
+    <div className="fixed z-20 flex items-center p-2 text-white bg-gray-800 border-t border-gray-700 rounded-lg shadow-lg bottom-14 md:bottom-0 left-1 right-1">
       <div
-        className="flex items-center space-x-2 flex-grow cursor-pointer"
+        className="flex items-center flex-grow space-x-2 cursor-pointer"
         onClick={handleCardClick}
       >
         <img
           src={currentSong.image}
           alt={currentSong.song}
-          className="w-10 h-10 object-cover rounded-lg shadow-md"
+          className="object-cover w-10 h-10 rounded-lg shadow-md"
         />
         <div className="flex flex-col justify-center overflow-hidden max-w-[calc(100% - 3rem)]">
-          <div className="relative w-40 sm:w-48 overflow-hidden">
+          <div className="relative w-40 overflow-hidden sm:w-48">
             <p className="font-bold truncate">{currentSong.song}</p>
             <p className="text-sm text-gray-300 truncate">
               {currentSong.singers ||
@@ -43,7 +43,7 @@ const MiniPlayer = () => {
       </div>
       <button
         onClick={togglePlayPause}
-        className="flex items-center justify-center w-10 h-10 sm:w-12 sm:h-12 ml-1 sm:ml-2 mr-3 bg-gray-700 text-white rounded-full shadow-md transition-transform transform hover:scale-105"
+        className="flex items-center justify-center w-10 h-10 ml-1 mr-3 text-white transition-transform transform bg-gray-700 rounded-full shadow-md sm:w-12 sm:h-12 sm:ml-2 hover:scale-105"
         title={isPlaying ? "Pause" : "Play"}
       >
         {isPlaying ? <FaPause /> : <FaPlay />}
